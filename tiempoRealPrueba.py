a60659f2deea1952f107ab6d8204a93a7a17b0f7
ryzust <zrubiol1700@alumno.ipn.mx>
2024-01-06 14:48:38 -0600

    model

--- tiempoRealPrueba.py
+++ tiempoRealPrueba.py
@@ -5,20 +5,32 @@ import pyaudio
 import wave
 
 # Cargar el modelo entrenado
-model = tf.keras.models.load_model('modelosAprendidos/pruebaCNN')
+model = tf.keras.models.load_model("modeloPrueba.h5")
+
 
 # Preprocesamiento de audio en tiempo real
-def preprocess_audio_real_time(audio, sr=22050):
+def preprocess_audio_real_time(audio, sr=22050, max_length_mfcc=128):
     duration = 2  # Duración deseada del audio en segundos
     # Si el audio es más largo que la duración especificada, se toma la parte central
     if len(audio) > sr * duration:
         start = (len(audio) - sr * duration) // 2
-        audio = audio[start:start + sr * duration]
+        audio = audio[start : start + sr * duration]
 
-    # Realizar preprocesamiento para obtener el espectrograma
-    mel_spec = librosa.feature.melspectrogram(y=audio, sr=sr)
-    log_mel_spec = librosa.power_to_db(mel_spec, ref=np.max)
-    return log_mel_spec
+    audio = audio.astype(np.float32)  # Convertir a float32 para normalización
+    audio /= np.max(np.abs(audio))  # Normalizar entre -1 y 1
+
+    # Extraer coeficientes MFCC
+    mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=max_length_mfcc)
+    mfcc = mfcc.astype(np.float32)
+
+    # Ajustar la longitud de los MFCC
+    if mfcc.shape[1] < max_length_mfcc:
+        pad_width = max_length_mfcc - mfcc.shape[1]
+        mfcc = np.pad(mfcc, ((0, 0), (0, pad_width)), mode="constant")
+    else:
+        mfcc = mfcc[:, :max_length_mfcc]  # Acortar si es más largo
+    return mfcc
+
 
 # Función para capturar y procesar audio en tiempo real
 def capture_audio_and_predict():
@@ -26,15 +38,13 @@ def capture_audio_and_predict():
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 22050
-    RECORD_SECONDS = 2
+    RECORD_SECONDS = 3
 
     p = pyaudio.PyAudio()
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
+    stream = p.open(
+        format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK
+    )
 
     print("* Comenzando la grabación...")
 
@@ -51,10 +61,13 @@ def capture_audio_and_predict():
     p.terminate()
 
     # Convertir los datos de audio capturados en un arreglo numpy
-    audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
+    audio_data = np.frombuffer(b"".join(frames), dtype=np.int16)
+
+    # Convertir audio a formato de punto flotante
+    audio_data_float = librosa.util.buf_to_float(audio_data, dtype=np.float32)
 
     # Preprocesar el audio en tiempo real
-    processed_audio = preprocess_audio_real_time(audio_data)
+    processed_audio = preprocess_audio_real_time(audio_data_float)
 
     # Ajustar la forma del audio para que sea compatible con el modelo
     processed_audio = np.expand_dims(processed_audio, axis=0)
@@ -62,15 +75,22 @@ def capture_audio_and_predict():
 
     # Realizar la predicción con el modelo cargado
     prediction = model.predict(processed_audio)
-    
+    confidence = np.max(prediction) * 100
+
+    print(prediction)
+
     # Obtener la clase predicha
     predicted_class = np.argmax(prediction)
-    
+
+    print(predicted_class)
+
     # Mapear la clase predicha a un hechizo específico según el orden de tus clases
-    spell_classes = ['Crucio', 'Desmayo', 'Imperio', 'Lumus', 'Protego', 'Reducto']
+    spell_classes = ["Crucio", "Desmayo", "Imperio", "Lumus", "Protego", "Reducto"]
     predicted_spell = spell_classes[predicted_class]
 
     print(f"Hechizo predicho: {predicted_spell}")
+    print(f"Porcentaje de confianza: {confidence:.2f}%")
+
 
 # Llamar a la función para capturar audio en tiempo real y hacer una predicción
 capture_audio_and_predict()
